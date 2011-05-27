c265d6d107d87114f5f6a18e843153d6f27d19e3
Anton Golov <kdesevis@gmail.com>
2011-05-27 21:48:49 +0200

    Wrote a base for the expression class.

--- expression/__init__.py
+++ expression/__init__.py
@@ -1,7 +1,11 @@
 """Provide classes that represent an expression tree.
 
 Classes:
-Expression  Represent a full expression.
+Expression  Represent a full expression
+BinaryOp    Represent a binary operator
+Constant    Represent an integer or real constant
+Variable    Represent a variable in an expression
+Function    Represent a mathematical function
 
 """
 
---
+++ expression/expression.py
@@ -0,0 +1,44 @@
+"""Provide the Expression class.
+
+"""
+
+class Expression(object):
+    """Represent a mathematical expression.
+
+    This class represents an interface that classes that can represent a full
+    mathematical expression (such as BinaryOp, Variable and Constant) should
+    follow.  By default, all methods except __repr__ raise a NotImplemented
+    exception, and must therefore be overriden in any subclasses.
+
+    Methods:
+        evaluate(self, variables)
+            Evaluate the expression and return the result.  
+
+        __str__(self)
+            Return a human-readable representation of the expression.
+
+        __repr__(self)
+            Return a more detailed representation of the expression.
+
+    """
+    def evaluate(self, variables):
+        """Evaluate the expression and return the result as a float.
+
+        Parameters:
+        variables   A dictionary of variable name and value pairs.
+
+        """
+        raise NotImplemented()
+
+    def __str__(self):
+        """Return a human-readable representation of the expression."""
+        raise NotImplemented()
+
+    def __repr__(self):
+        """Return a more detailed representation of the expression.
+
+        It is convenient if evaluating this string would create an identical
+        instance of the expression, but this is not necessary.
+        """
+        return "Expression()"
+
