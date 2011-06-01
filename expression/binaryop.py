0d3d1b762a93c602e82a3ad570e063080ab3467d
Anton Golov <kdesevis@gmail.com>
2011-06-01 22:15:06 +0200

    Added some more binary operations.

--- expression/binaryop.py
+++ expression/binaryop.py
@@ -92,3 +92,114 @@ class SumOp(BinaryOp):
 
     def __repr__(self):
         return "SumOp({0!r}, {1!r})".format(self.first, self.second)
+
+
+class DifferenceOp(BinaryOp):
+    """Represent an expression that is a difference of two expressions.
+
+    For details, see the BinaryOp docstring.
+
+    Important differences:
+
+    Methods:
+        __init__(self, first, second)
+            Initialise the attributes; the operator is not necessary.
+
+        evaluate(self)
+            Return the difference of first and second.
+
+    """
+    def __init__(self, first, second):
+        """Initialise the attributes."""
+        # Putting spaces around the - for readability.
+        BinaryOp.__init__(self, first, " - ", second)
+
+    def evaluate(self):
+        """Return the difference of first and second."""
+        return first.evaluate() - second.evaluate()
+
+    def __repr__(self):
+        return "DiffOp({0!r}, {1!r})".format(self.first, self.second)
+
+
+class ProductOp(BinaryOp):
+    """Represent an expression that is a product of two expressions.
+
+    For details, see the BinaryOp docstring.
+
+    Important differences:
+
+    Methods:
+        __init__(self, first, second)
+            Initialise the attributes; the operator is not necessary.
+
+        evaluate(self)
+            Return the product of first and second.
+
+    """
+    def __init__(self, first, second):
+        """Initialise the attributes."""
+        BinaryOp.__init__(self, first, "*", second)
+
+    def evaluate(self):
+        """Return the product of first and second."""
+        return first.evaluate() * second.evaluate()
+
+    def __repr__(self):
+        return "ProductOp({0!r}, {1!r})".format(self.first, self.second)
+
+
+class QuotientOp(BinaryOp):
+    """Represent an expression that is a quotient of two expressions.
+
+    For details, see the BinaryOp docstring.
+
+    Important differences:
+
+    Methods:
+        __init__(self, first, second)
+            Initialise the attributes; the operator is not necessary.
+
+        evaluate(self)
+            Return the quotient of first and second.
+
+    """
+    def __init__(self, first, second):
+        """Initialise the attributes."""
+        BinaryOp.__init__(self, first, "/", second)
+
+    def evaluate(self):
+        """Return the product of first and second."""
+        return first.evaluate() / second.evaluate()
+
+    def __repr__(self):
+        return "QuotientOp({0!r}, {1!r})".format(self.first, self.second)
+
+
+class PowerOp(BinaryOp):
+    """Represent an expression that is one expression to the power of another.
+
+    For details, see the BinaryOp docstring.
+
+    Important differences:
+
+    Methods:
+        __init__(self, first, second)
+            Initialise the attributes; the operator is not necessary.
+
+        evaluate(self)
+            Return the first to the power of the second.
+
+    """
+    def __init__(self, first, second):
+        """Initialise the attributes."""
+        BinaryOp.__init__(self, first, "^", second)
+
+    def evaluate(self):
+        """Return the first to the power of the second."""
+        return first.evaluate() ** second.evaluate()
+
+    def __repr__(self):
+        return "PowerOp({0!r}, {1!r})".format(self.first, self.second)
+
+
